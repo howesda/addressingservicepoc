42d9f7eb6555dc3aac89f80a813ae7d612b2e79e
Nick Martin <nick.martin@dwp.gsi.gov.uk>
2016-04-01 10:49:16 +0100

    Completed initial DoGetAddress QAS call using OS Places

---
+++ src/main/java/uk/gov/dwp/service/osplaces/uprn/ResponseResult.java
@@ -0,0 +1,24 @@
+package uk.gov.dwp.service.osplaces.uprn;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ResponseResult {
+	private ResponseResultDpa dpa;
+
+	/**
+	 * @return the dpa
+	 */
+	public ResponseResultDpa getDpa() {
+		return dpa;
+	}
+
+	/**
+	 * @param dpa the dpa to set
+	 */
+	@JsonProperty("DPA")
+	public void setDpa(ResponseResultDpa dpa) {
+		this.dpa = dpa;
+	}
+}
