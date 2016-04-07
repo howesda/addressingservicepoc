c47f73f1255a7d19867fde75390249bc25c308cd
David Howes <david-.howes@dwp.gsi.gov.uk>
2016-04-07 09:37:36 +0100

    Slightly shaky error handling - to be immediately refactored

--- src/main/java/uk/gov/dwp/service/osplaces/ExceptionProcessor.java
+++ src/main/java/uk/gov/dwp/service/osplaces/ExceptionProcessor.java
@@ -1,26 +1,15 @@
 package uk.gov.dwp.service.osplaces;
 
-import javax.ws.rs.core.Response;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import uk.gov.dwp.service.ErrorMessage;
-
 public class ExceptionProcessor implements Processor {
 	
-	private ObjectMapper mapper = new ObjectMapper();
-
 	@Override
 	public void process(Exchange exchange) throws Exception {
 		Exception exception = (Exception) exchange.getProperty(Exchange.EXCEPTION_CAUGHT);
-		
-		ErrorMessage message = new ErrorMessage(exception.getMessage());
 
-		Response response = Response.serverError().entity(mapper.writeValueAsString(message)).build();
-        exchange.getOut().setBody(response);
+        exchange.setProperty("Exception", exception);
 		
 	}
 }
