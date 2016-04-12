66d3007fc4f16e893aa34b10d02ffbb3a519e7c0
David Howes <david-.howes@dwp.gsi.gov.uk>
2016-04-12 12:03:02 +0100

    Changes to supply the stub's auth key and to pass back the http status of the remote call to the client

--- src/main/java/uk/gov/dwp/service/osplaces/RESTExceptionResponseSetter.java
+++ src/main/java/uk/gov/dwp/service/osplaces/RESTExceptionResponseSetter.java
@@ -1,22 +1,32 @@
 package uk.gov.dwp.service.osplaces;
 
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.http.common.HttpOperationFailedException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import uk.gov.dwp.service.ErrorMessage;
+
 
 public class RESTExceptionResponseSetter implements ExceptionResponseSetter {
 	
 	@Override
 	public void setResponse(Exchange exchange,Exception exception) throws Exception {
 		ErrorMessage message = new ErrorMessage(exception.getMessage());
+		
+		ResponseBuilder responseBuilder = Response.serverError();
+		
+		if (exception instanceof HttpOperationFailedException){
+			HttpOperationFailedException httpEx = (HttpOperationFailedException)exception;
+			responseBuilder = Response.status(httpEx.getStatusCode());
+		}
 
 		ObjectMapper mapper = new ObjectMapper();
 
-		Response response = Response.serverError().entity(mapper.writeValueAsString(message)).build();
+		Response response = responseBuilder.entity(mapper.writeValueAsString(message)).build();
 		exchange.getOut().setBody(response);		
 
 	}
