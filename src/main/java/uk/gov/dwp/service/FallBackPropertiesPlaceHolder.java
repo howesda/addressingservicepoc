0fe4b09e9a64e4368f869a60f25067923ac751e7
David Howes <david-.howes@dwp.gsi.gov.uk>
2016-05-16 11:32:23 +0100

    Added FallBack properties class

---
+++ src/main/java/uk/gov/dwp/service/FallBackPropertiesPlaceHolder.java
@@ -0,0 +1,52 @@
+package uk.gov.dwp.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.camel.component.properties.PropertiesFunction;
+
+public class FallBackPropertiesPlaceHolder implements PropertiesFunction {
+
+	private final static String defaultPropsFileLocation = "/route.properties";
+	
+	private Properties props = new Properties();
+
+	
+	public FallBackPropertiesPlaceHolder(){
+		String propsFileLocation = System.getProperty("route.props");
+		System.out.println("**********************************************************");
+		System.out.println(" FallBackPropertiesPlaceHolder propsfilelocation = " + propsFileLocation);
+		System.out.println("**********************************************************");
+
+		try {
+
+			InputStream is = null;
+			if ( propsFileLocation == null ){
+				is = FallBackPropertiesPlaceHolder.class.getResourceAsStream(defaultPropsFileLocation);
+			} else {
+				is = new FileInputStream(new File(propsFileLocation));
+			}
+				
+			props.load(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+	
+	@Override
+	public String getName() {
+		return "props";
+	}
+
+	@Override
+	public String apply(String key) {
+		return props.getProperty(key);
+	}
+
+}
